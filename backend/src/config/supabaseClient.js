04e92bebabc72670b994693b4a98bcdf4b209253
RD1707 <souplemonpie@gmail.com>
2025-08-25 18:11:22 -0300

    first commit

--- backend/src/config/supabaseClient.js
+++ backend/src/config/supabaseClient.js
@@ -1,7 +1,6 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = process.env.SUPABASE_URL;
-// Confirme que esta linha usa SUPABASE_SERVICE_ROLE_KEY
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 console.log("INICIALIZANDO CLIENTE SUPABASE. A CHAVE ESTÁ DEFINIDA?", !!supabaseKey);navigator
